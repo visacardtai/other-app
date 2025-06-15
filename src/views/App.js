e87ec7fcc3c0fad5d105940e0c885154ffea0142
visacardtai <tientai772001@gmail.com>
2025-06-15 16:50:39 +0700

    15/06

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,6 @@
-import './App.scss';
-import { useState } from 'react';
-import First from '../components/First';
+import "./App.scss";
+import { useState } from "react";
+import First from "../components/First";
 
 function App() {
   const [name, setName] = useState("tai");
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <First />
+        Hahaha
       </header>
     </div>
   );
